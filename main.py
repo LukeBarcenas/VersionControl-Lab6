11467bc3d7dac8de8203dae889eda3b0f1e4254a
Lukondrias <lukework930@gmail.com>
2023-10-26 14:53:22 -0400

    Added comments

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Luke Barcenas
 
+# encoder function, adds 3 to each number in the password
 def encoder(password):
     encoded_pass = []
+    # goes through each number in the string, adds 3, and
+    # puts them in a new list
     for i in range(len(password)):
         temp_char = 0
         temp_char = int(password[i]) + 3
@@ -10,6 +13,7 @@ def encoder(password):
             encoded_pass.append(temp_char)
         else:
             encoded_pass.append(temp_char)
+    # the list is made into a string
     encoded_pass = "".join([str(i) for i in encoded_pass])
     return encoded_pass
 
@@ -17,6 +21,7 @@ def encoder(password):
 if __name__ == "__main__":
     user_input = 0
 
+    # menu loop
     while True:
         print("\nMenu\n-------------")
         print("1. Encode\n"
@@ -24,13 +29,16 @@ if __name__ == "__main__":
               "3. Quit\n\n")
         user_input = input("Please enter an option: ")
 
+        # if the user types '1', run the encoder
         if user_input == "1":
             user_pass = input("Please enter your password to encode: ")
             encoded_password = encoder(user_pass)
             print("Your password has been encoded and stored!\n\n")
 
+        # if the user types '2', run the decoder
         elif user_input == "2":
             pass  # call to decoder function goes here
 
+        # if the user types '3', end the program
         elif user_input == "3":
             break
